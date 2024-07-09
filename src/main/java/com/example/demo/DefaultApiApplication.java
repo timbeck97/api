88c97c5ea10abcf4407764360df9092f88684b29
Tim Morgenstern <tim@infisc.com.br>
2024-07-08 22:39:46 -0300

    first

---
+++ src/main/java/com/example/demo/DefaultApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefaultApiApplication.class, args);
+	}
+
+}
